479aa01d9e1d48fbe1c8900fc8ea4f6737333be8
ggyu777 <73706252+ggyu777@users.noreply.github.com>
2022-02-12 18:10:51 +0900

    첫 커밋
    ㅇㅇ

---
+++ src/Sibal.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SibalUp from './components/SibalUp'
+
+const Sibal = () =>{
+    return(
+        <SibalUp name1 = {this.props.location.name}>
+        </SibalUp>
+    );
+
+    
+}
+export default Sibal
---
+++ src/Userinfo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUp from './components/SignUp.js'
+
+const Userinfo = () => {
+        return(
+            <SignUp >
+              </SignUp>
+        );
+      }
+export default Userinfo
+
+//h
