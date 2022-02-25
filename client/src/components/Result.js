79750fd6f4a74a6bbd02a4a378b810cd061edd10
ggyu777 <12161655@inha.edu>
2022-02-25 23:42:40 +0900

    kk

--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -8,47 +8,44 @@ export default function Result()
 
     const birthYear1 = location.state.birthYear;
     var birthMonth1 = location.state.birthMonth;
-    if (birthMonth1<10)
-    {
-      birthMonth1 = '0' + birthMonth1;
-    }
+        if (birthMonth1<10)
+        {
+          birthMonth1 = '0' + birthMonth1;
+        }
     var birthDay1 = location.state.birthDay;
-    if( birthDay1<10 )
-    {
-      birthDay1 = '0' + birthDay1;
-    }
+        if( birthDay1<10 )
+        {
+          birthDay1 = '0' + birthDay1;
+        }
 
-    let params = { solYear: birthYear1, solMonth : birthMonth1 ,solDay : birthDay1 };
-    //let query = encodeURIComponent('lunYear') + '=' + encodeURIComponent("1996")+'&'+ encodeURIComponent('lunMonth') + '=' + encodeURIComponent("09") + '&'+encodeURIComponent('lunDay') + '=' + encodeURIComponent("18");
-    let query = Object.keys(params) .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k])) .join('&');
-
-    let url = 'http://localhost:5000/api?' + 'serviceKey=Tcv1D3HbX2tvmM9kiulXfVycMLwOvXeTrVDbaCOMDhLabVxSuz0oyd%2FviaMRqdfA%2FpRdHQnCtO3GyupkmZBbmw%3D%3D'+'&'+ query;
+    const flag1 = location.state.flag;
 
     
-
+    let params = { solYear: birthYear1, solMonth : birthMonth1 ,solDay : birthDay1 };
+    let query = Object.keys(params) .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k])) .join('&');
+    let url = 'http://localhost:5000/api?' + 'serviceKey=Tcv1D3HbX2tvmM9kiulXfVycMLwOvXeTrVDbaCOMDhLabVxSuz0oyd%2FviaMRqdfA%2FpRdHQnCtO3GyupkmZBbmw%3D%3D'+'&'+ query;
+    
+    
     const [ilju,setIlju] = useState('');
-
 
     const name1 = location.state.username;
     const birthday = location.state.birthDate;
-    const flag1 = location.state.flag;
 
 //.data.elements[0].elements[1].elements[0].elements[0].elements[1].elements[0].text)
 
     const SorL = (flag1 ? "양력" : "음력")
     console.log(birthday)
  
+    let ismunt = true;
 
- //   .then((data) => console.log(elements[0].elements[1].elements[0].elements[0].elements[1].elements[0].text))
     const callApi = async () => {
-        axios.get(url,
-        ).then((res)=> {
-        console.log(setIlju(res.data.elements[0].elements[1].elements[0].elements[0].elements[1].elements[0].text));
-      })
+      axios.get(url).then((res)=>console.log(setIlju(res.data.elements[0].elements[1].elements[0].elements[0].elements[1].elements[0].text)))
       };
     
       useEffect(()=>{
-        callApi();
+   
+          callApi();
+  
       },[]);
     
 
