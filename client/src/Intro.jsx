e5325ba5881a534cab48f8671d3b3470a15ce02a
ggyu777 <12161655@inha.edu>
2022-03-07 16:38:21 +0900

    폰트 적용

--- client/src/Intro.jsx
+++ client/src/Intro.jsx
@@ -10,28 +10,29 @@ const Intro = () => {
         window.location.href = "/user"
     }
     // 클릭 시 /user로 링크 이동
-
+//
     return (
         
         <main>
             
             <div class="card"> 
            
-            <img src = {IntroIMG} style={{maxWidth:'27vw'}} />
+            <img src = {IntroIMG} style={{maxWidth:'520px'}} />
                 <div class="text-wrap">
-                        <Container  style={{marginTop: '100px'}}> 
-                            
-                            <Typography variant="h3" align='center' color='textPrimary' gutterBottom>
-                                사주로 보는 나는?
-                            </Typography>
 
-                            <Typography variant="h1" align='center' style={{marginTop: '100px'}}>
+                <p><span id='HSYouji_font'>
+                                    ~사주로 알아보는~ <br/>
+                            </span></p>
+                <p><span id='HSYouji_font' fontSize='10px'>
+                                    당신에게 주어진 두가지 기운
+                            </span></p>
+
+                            <Typography variant="h1" align='center' >
                                 <Button variant="contained" onClick={handleClick} endIcon={<SendIcon />}>
                                     보러가기
                                 </Button>
                             </Typography>
 
-                        </Container>
                 </div>
             </div>
         </main>
