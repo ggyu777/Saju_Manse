21bec5ce9ad9042e46db91ab01ab778471a4dc28
ggyu777 <12161655@inha.edu>
2022-03-27 18:20:09 +0900

    d

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+  entry: "./src/test.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname + "/build"),
+  },
+  mode: "none",
+};
